093d37c6e9626e94f2fefd593b45083bb4922a8e
MahenPD <61178225+MahenPD@users.noreply.github.com>
2021-01-15 23:54:43 +0530

    bug fixed on the banner

--- src/App.js
+++ src/App.js
@@ -11,13 +11,13 @@ function App() {
       <Banner />
 
       <Row title="NETFLIX ORIGINALS" isLargeRow fetchUrl={requests.fetchNetflixOriginals} />
-      <Row title="TRENDING NOW" fetchUrl={requests.fetchTrending} />
-      <Row title="TOP RATED" fetchUrl={requests.fetchTopRated} />
-      <Row title="ACTION MOVIES" fetchUrl={requests.fetchActionMovies} />
-      <Row title="HORROR MOVIES" fetchUrl={requests.fetchHorrorMovies} />
-      <Row title="COMEDY MOVIES" fetchUrl={requests.fetchComedyMovies} />
-      <Row title="ROMANCE MOVIES" fetchUrl={requests.fetchRomanceMovies} />
-      <Row title="DOCUMENTARIES" fetchUrl={requests.fetchDocumentaries} />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
